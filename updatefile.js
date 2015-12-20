d9ca28f5e70b20d3b92edf1b8bdc02f2668ca4ed
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-19 21:18:45 -0500

    adds logger, and `reflinks`, `htmlComments` and `license` middlewares

--- updatefile.js
+++ updatefile.js
@@ -9,12 +9,21 @@ module.exports = function(app, base, env) {
     middleware.verbrc,
   ]));
 
+  function logger(file, next) {
+    base.log('updating readme');
+    next();
+  }
+
   base.onStream(/\.(verb|readme)\.md$/i, utils.series([
+    logger,
     middleware.lintDeps,
+    middleware.htmlComments,
     middleware.install,
     middleware.related(base),
+    middleware.reflinks(base),
     middleware.travis(base),
     middleware.jscomments,
+    middleware.license,
     middleware.whitespace
   ]));
 };
