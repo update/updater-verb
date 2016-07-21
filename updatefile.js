fc8e9edce879a43bafc96cc447991e0d63dd0bf1
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-21 16:12:08 -0400

    organize code

--- updatefile.js
+++ updatefile.js
@@ -1,29 +1,46 @@
 'use strict';
 
-var utils = require('middleware-utils');
-var middleware = require('./');
+var middleware = require('./lib/middleware');
+var utils = require('./lib/utils');
 
 module.exports = function(app, base, env) {
-  // rename `.verbrc.md` to `.verb.md`
-  base.onLoad(/\.verbrc\.md$/, utils.series([
-    middleware.verbrc,
-  ]));
 
-  function logger(file, next) {
-    base.log('updating readme');
-    next();
-  }
+  app.task('verb', function() {
+    return app.src('.verb*.md')
+      .pipe(utils.through.obj(function(file, enc, next) {
+        utils.del(file.path, function(err, files) {
+          if (err) {
+            next(err);
+            return;
+          }
+          if (file.basename !== '.verb.md') {
+            app.log.success('renamed', file.relative);
+          }
+          file.basename = '.verb.md';
+          next(null, file);
+        });
+      }))
+      .pipe(utils.series([
+        middleware.lintDeps,
+        middleware.htmlComments,
+        middleware.install,
+        middleware.related(base),
+        middleware.reflinks(base),
+        middleware.travis(base),
+        middleware.jscomments,
+        middleware.license,
+        middleware.whitespace,
+        middleware.toGithubUrl
+      ]))
+      .pipe(app.dest(app.cwd));
+  });
 
-  base.onStream(/\.(verb|readme)\.md$/i, utils.series([
-    logger,
-    middleware.lintDeps,
-    middleware.htmlComments,
-    middleware.install,
-    middleware.related(base),
-    middleware.reflinks(base),
-    middleware.travis(base),
-    middleware.jscomments,
-    middleware.license,
-    middleware.whitespace
-  ]));
+  app.task('default', ['verb']);
 };
+
+function logger(msg) {
+  return function(file, next) {
+    console.log(msg + ' readme');
+    next();
+  };
+}
